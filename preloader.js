86129ea60d63b1280f14aad4460f4572476c1bda
Louislf <lefrancois_louis@yahoo.fr>
2024-05-26 18:43:23 +0200

    up

--- preloader.js
+++ preloader.js
@@ -5,28 +5,3 @@ window.addEventListener("load", function () {
     loader.style.display = "none";
   }, 500);
 });
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   window.addEventListener('load', function() {
-//       function Preloader() {
-//           var preloader = document.querySelector('.loader');
-//           setTimeout(function() {
-//               preloader.classList.add('fadeOut');
-//               setTimeout(function() {
-//                   preloader.style.display = 'none';
-//               }, 500);
-//           }, 1000);
-//       }
-
-//       if (!sessionStorage.getItem('doNotShow')) {
-//           sessionStorage.setItem('doNotShow', 'true');
-//           Preloader();
-//       } else {
-//           var preloader = document.querySelector('.loader');
-//           if (preloader) {
-//               preloader.style.display = 'none';
-//           }
-//       }
-//   });
-// });
