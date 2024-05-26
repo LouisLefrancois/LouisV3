532d2be68f3def471c9eef39932c81ac7f9ed3d5
Louislf <lefrancois_louis@yahoo.fr>
2024-05-26 18:53:50 +0200

    up

--- scroll-bar-index.js
+++ scroll-bar-index.js
@@ -6,9 +6,9 @@ window.addEventListener('scroll', function() {
   
     var scrollPosition = window.scrollY; // scrollPosition = pos fenêtre
     var windowHeight = document.documentElement.scrollHeight - window.innerHeight; // windowHeight = hauteur totale de la page moins la hauteur de la fenêtre visible
-    var scrollPercentage = (scrollPosition / windowHeight) * 100; // scrollPercentage = % de défilement par rapport à la hauteur totale de la page
+    var scrollPercentage = (scrollPosition / windowHeight) * 100; // calcul le % de défilement : divise la pos du scroll actuel par la hauteur totale de la page, et en multipliant par 100 (pour %)
 
     var scrollBar = document.querySelector('.scroll-bar');
 
-    scrollBar.style.height = scrollPercentage + '%'; // hauteur de scrollBar = % de défilement (en %)
+    scrollBar.style.height = scrollPercentage + '%'; // définir la hauteur du scroll en %
   });
--- src/scroll-bar.js
+++ src/scroll-bar.js
@@ -1,14 +1,9 @@
 window.addEventListener("scroll", function () {
-  var scrollPosition = window.scrollY;
-
-  // récupère hauteur totale de la page
-  var windowHeight = document.documentElement.scrollHeight - window.innerHeight;
-
-  // calcul le % de défilement : divise la pos du scroll actuel par la hauteur totale de la page, et en multipliant par 100 (pour %)
-  var scrollPercentage = (scrollPosition / windowHeight) * 100;
-
+  var scrollPosition = window.scrollY; // scrollPosition = pos fenêtre
+  var windowHeight = document.documentElement.scrollHeight - window.innerHeight; // windowHeight = hauteur totale de la page moins la hauteur de la fenêtre visible
+  var scrollPercentage = (scrollPosition / windowHeight) * 100; // calcul le % de défilement : divise la pos du scroll actuel par la hauteur totale de la page, et en multipliant par 100 (pour %)
+  
   var scrollBar = document.querySelector(".scroll-bar");
 
-  // définir la hauteur du scroll en %
-  scrollBar.style.height = scrollPercentage + "%";
+  scrollBar.style.height = scrollPercentage + "%"; // définir la hauteur du scroll en %
 });
