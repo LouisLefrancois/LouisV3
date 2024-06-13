daf0a383530610f5f52ea8395295f9bd9a17d5c2
Louislf <lefrancois_louis@yahoo.fr>
2024-06-13 19:25:48 +0200

    Update gsap-contact.js

--- src/gsap-contact.js
+++ src/gsap-contact.js
@@ -2,7 +2,7 @@ const firstline = document.querySelector('.first-line');
 const secondline = document.querySelector('.second-line');
 const mailcontainer = document.querySelector('.mail-container');
 
-function startAnimations() {
+document.addEventListener('DOMContentLoaded', function() {
   gsap.timeline({
     onComplete: function() {
       firstline.style.visibility = 'visible';
@@ -10,29 +10,18 @@ function startAnimations() {
       mailcontainer.style.visibility = 'visible';
     }
   });
-
-  gsap.set('.first-section', {});
-
-  gsap.timeline().from('.first-section', {})
-    .from('.first-line', { duration: 1, x: -window.innerWidth, ease: 'power2.out' }, 0)
-    .from('.second-line', { duration: 1.4, x: -window.innerWidth, ease: 'power2.out' }, 0)
-    .from('.mail-container', { duration: 1.75, x: -window.innerWidth, ease: 'power2.out' }, 0);
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  startAnimations();
 });
 
-window.addEventListener('pageshow', function(event) {
-  if (event.persisted) {
-    // La page a été restaurée depuis le cache
-    startAnimations();
-  }
-});
+gsap.set('.first-section', {});
+
+gsap.timeline().from('.first-section', {})
+  .from('.first-line', { duration: 1, x: -window.innerWidth, ease: 'power2.out' }, 0)
+  .from('.second-line', { duration: 1.4, x: -window.innerWidth, ease: 'power2.out' }, 0)
+  .from('.mail-container', { duration: 1.75, x: -window.innerWidth, ease: 'power2.out' }, 0)
 
 function goToPage(url) {
-  gsap.to('.first-line', { duration: 1, x: -window.innerWidth, ease: 'power2.inOut'}, );
-  gsap.to('.second-line', { duration: 1.15, x: -window.innerWidth, ease: 'power2.inOut'}, );
+  gsap.to('.first-line', { duration: 1, x: -window.innerWidth, ease: 'power2.inOut'}, )
+  gsap.to('.second-line', { duration: 1.15, x: -window.innerWidth, ease: 'power2.inOut'}, )
   gsap.to('.mail-container', { duration: 1.3, x: -window.innerWidth, ease: 'power2.inOut', onComplete: () => { window.location.href = url; }
   });
 }
@@ -41,33 +30,6 @@ document.querySelectorAll('.menu a').forEach(link => {
   link.addEventListener('click', function(event) {
     event.preventDefault(); // empêche le comportement par défaut du lien
     const url = this.getAttribute('href'); // récupère lien
-    goToPage(url); // déclenche la transition de page
-  });
-});
-
-// Écouteur d'événement pour détecter le retour en arrière du navigateur
-window.addEventListener('popstate', function(event) {
-  // Vérifie si l'événement popstate est déclenché par un retour en arrière du navigateur
-  if (event.state && event.state.isBack) {
-    startAnimations(); // Redémarre les animations lors du retour en arrière
-  }
-});
-
-// Fonction pour ajouter l'état de navigation
-function goToPageWithState(url) {
-  gsap.to('.first-line', { duration: 1, x: -window.innerWidth, ease: 'power2.inOut'}, );
-  gsap.to('.second-line', { duration: 1.15, x: -window.innerWidth, ease: 'power2.inOut'}, );
-  gsap.to('.mail-container', { duration: 1.3, x: -window.innerWidth, ease: 'power2.inOut', onComplete: () => {
-    history.pushState({ isBack: true }, '', ''); // Ajoute un état de navigation
-    window.location.href = url;
-  }});
-}
-
-// Mise à jour des écouteurs d'événements avec la nouvelle fonction goToPageWithState
-document.querySelectorAll('.menu a').forEach(link => {
-  link.addEventListener('click', function(event) {
-    event.preventDefault(); // empêche le comportement par défaut du lien
-    const url = this.getAttribute('href'); // récupère le lien
-    goToPageWithState(url); // déclenche la transition de page avec état de navigation
+    goToPage(url); // déclanche la transition de page
   });
 });
