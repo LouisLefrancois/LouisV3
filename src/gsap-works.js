31825437a90176411b12ade254cbbc794146ed60
Louislf <lefrancois_louis@yahoo.fr>
2024-06-13 21:53:06 +0200

    Update gsap-works.js

--- src/gsap-works.js
+++ src/gsap-works.js
@@ -1,4 +1,11 @@
 const textsection = document.querySelector('.text-section');
+const transition = document.querySelector('.transition');
+
+function resetAnimations() {
+  // Réinitialiser les propriétés des éléments animés
+  gsap.set(textsection, { clearProps: "all" });
+  gsap.set(transition, { clearProps: "all" });
+}
 
 function startAnimations() {
   gsap.timeline({
@@ -22,6 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
 window.addEventListener('pageshow', function(event) {
   if (event.persisted) {
     // La page a été restaurée depuis le cache
+    resetAnimations(); // Réinitialiser les propriétés avant de relancer les animations
     startAnimations();
   }
 });
@@ -35,6 +43,38 @@ document.querySelectorAll('.logo a, .menu a, .thanks a.next-project').forEach(li
   link.addEventListener('click', function (event) {
     event.preventDefault(); // empêche le comportement par défaut du lien
     const url = this.getAttribute('href'); // récupère lien
-    goToPage(url); // déclanche la transition de page
+    goToPage(url); // déclenche la transition de page
+  });
+});
+
+// Écouteur d'événement pour détecter le retour en arrière du navigateur
+window.addEventListener('popstate', function(event) {
+  // Vérifie si l'événement popstate est déclenché par un retour en arrière du navigateur
+  if (event.state && event.state.isBack) {
+    resetAnimations(); // Réinitialiser les propriétés avant de relancer les animations
+    startAnimations(); // Redémarre les animations lors du retour en arrière
+  }
+});
+
+// Fonction pour ajouter l'état de navigation
+function goToPageWithState(url) {
+  gsap.to('.transition', {
+    duration: 1,
+    scaleY: 1,
+    transformOrigin: 'bottom',
+    ease: 'power4.inOut',
+    onComplete: () => {
+      history.pushState({ isBack: true }, '', ''); // Ajoute un état de navigation
+      window.location.href = url;
+    }
+  });
+}
+
+// Mise à jour des écouteurs d'événements avec la nouvelle fonction goToPageWithState
+document.querySelectorAll('.logo a, .menu a, .thanks a.next-project').forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault(); // empêche le comportement par défaut du lien
+    const url = this.getAttribute('href'); // récupère le lien
+    goToPageWithState(url); // déclenche la transition de page avec état de navigation
   });
 });
