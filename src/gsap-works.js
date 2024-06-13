6cdacb03a03ab55337defc9c77faf7c1cb97b4a3
Louislf <lefrancois_louis@yahoo.fr>
2024-06-13 18:20:13 +0200

    Update gsap-works.js

--- src/gsap-works.js
+++ src/gsap-works.js
@@ -1,32 +1,29 @@
 const textsection = document.querySelector('.text-section');
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Set session storage item to indicate Page2 has been visited
-    sessionStorage.setItem("Page2Visited", "True");
-
-    // GSAP animation for textsection
-    gsap.timeline({
-        onComplete: function () {
-            textsection.style.visibility = 'visible';
-            textsection.style.mixBlendMode = 'difference';
-        }
-    });
+  gsap.timeline({
+    onComplete: function () {
+      textsection.style.visibility = 'visible';
+      textsection.style.mixBlendMode = 'difference';
+    }
+  });
 });
 
 if (window.innerWidth > 768) {
-    gsap.timeline().from('.text-section', { duration: 1, x: window.innerWidth, ease: 'power2.out' }, 1.2);
+  gsap.timeline().from('.text-section', { duration: 1, x: window.innerWidth, ease: 'power2.out' }, 1.2);
 }
 
-gsap.timeline().from('.transition', { duration: 1.2, scaleY: -1, y: -window.innerHeight, transformOrigin: 'bottom', ease: 'power4.inOut' }, .5);
+gsap.timeline().from('.transition', { duration: 1.2, scaleY: -1, y: -window.innerHeight, transformOrigin: 'bottom', ease: 'power4.inOut' }, .5)
 
 function goToPage(url) {
-    gsap.to('.transition', { duration: 1, scaleY: 1, transformOrigin: 'bottom', ease: 'power4.inOut', onComplete: () => { window.location.href = url; }});
+  gsap.to('.transition', { duration: 1, scaleY: 1, transformOrigin: 'bottom', ease: 'power4.inOut', onComplete: () => { window.location.href = url; }
+  });
 }
 
 document.querySelectorAll('.logo a, .menu a, .thanks a.next-project').forEach(link => {
-    link.addEventListener('click', function (event) {
-        event.preventDefault(); // empêche le comportement par défaut du lien
-        const url = this.getAttribute('href'); // récupère lien
-        goToPage(url); // déclanche la transition de page
-    });
+  link.addEventListener('click', function (event) {
+    event.preventDefault(); // empêche le comportement par défaut du lien
+    const url = this.getAttribute('href'); // récupère lien
+    goToPage(url); // déclanche la transition de page
+  });
 });
