d3fd365e4955df839befd8c93b22b44a18f5e3f3
Louislf <lefrancois_louis@yahoo.fr>
2024-06-13 17:53:09 +0200

    Update gsap-index.js

--- gsap-index.js
+++ gsap-index.js
@@ -1,37 +1,30 @@
 const fullcontainer = document.querySelectorAll('.full-container');
-    const navbar = document.querySelector('.navbar');
-    const scrolldowntxt = document.querySelector('.scroll-down');
-    const buttondarkmode = document.querySelector('.buttondarkmode');
+const navbar = document.querySelector('.navbar');
+const scrolldowntxt = document.querySelector('.scroll-down');
+const buttondarkmode = document.querySelector('.buttondarkmode');
 
-    function playAnimation() {
-      const TL = gsap.timeline({ paused: true });
-      TL
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({ paused: true });
+
+    TL
         .from(navbar, .2, { top: 10, opacity: 0 }, .5) //  durée d'exec , sec avant exec
         .from(fullcontainer, .75, { opacity: 0 }, 1) 
         .from(scrolldowntxt, .75, { opacity: 0 }, 1.5) 
         .from(buttondarkmode, .75, { opacity: 0 }, 1.5) 
-      TL.play();
-    }
+    TL.play();
+});
 
-    window.addEventListener('load', playAnimation);
-
-    function goToPage(url) {
-      gsap.to('.full-container, .scroll-down', { duration: .75, x: -window.innerWidth, opacity: 0, ease: 'power2.inOut', onComplete: () => {
-        window.location.href = url;
-      }});
-    }
-    
-    document.querySelectorAll('.menu a, .full-container a').forEach(link => {
-      link.addEventListener('click', function(event) {
-        event.preventDefault(); // empêche le comportement par défaut du lien
-        const url = this.getAttribute('href'); // récupère lien
-        goToPage(url); // déclanche la transition de page
-      });
+function goToPage(url) {
+    gsap.to('.full-container, .scroll-down', { duration: .75, x: -window.innerWidth, opacity: 0, ease: 'power2.inOut', onComplete: () => {
+      window.location.href = url;
+    }});
+  }
+  
+  document.querySelectorAll('.menu a, .full-container a').forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault(); // empêche le comportement par défaut du lien
+      const url = this.getAttribute('href'); // récupère lien
+      goToPage(url); // déclanche la transition de page
     });
-
-    document.addEventListener('visibilitychange', () => {
-      if (document.visibilityState === 'visible') {
-        gsap.set([navbar, fullcontainer, scrolldowntxt, buttondarkmode], { opacity: 0 });
-        playAnimation();
-      }
-    });
+  });
