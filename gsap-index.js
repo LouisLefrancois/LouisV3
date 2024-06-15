0742c6e43a5c5d0860ca1531a2149a0da7f87f0c
Louislf <lefrancois_louis@yahoo.fr>
2024-06-15 19:06:50 +0200

    Update gsap-index.js

--- gsap-index.js
+++ gsap-index.js
@@ -3,12 +3,14 @@ const navbar = document.querySelector('.navbar');
 const scrolldowntxt = document.querySelector('.scroll-down');
 const buttondarkmode = document.querySelector('.buttondarkmode');
 
-function resetFullContainers() {
+// reset fullcontainer
+function resetFullContainer() {
   fullcontainer.forEach(container => {
     gsap.set(container, { x: 0, opacity: 1 });
   });
 }
 
+// exec les anims
 function playTimeline() {
   const TL = gsap.timeline({ paused: true });
   TL
@@ -19,11 +21,13 @@ function playTimeline() {
   TL.play();
 }
 
+// exec les fonctions au chargement de la page
 window.addEventListener('load', () => {
-  resetFullContainers();
+  resetFullContainer();
   playTimeline();
 });
 
+// anim quand on part de la page, (quand on clique sur .menu a ou .full-container a)
 function goToPage(url, element) {
   gsap.to(element, { duration: .75, x: -window.innerWidth, opacity: 0, ease: 'power2.inOut', onComplete: () => {
     window.location.href = url;
@@ -32,17 +36,17 @@ function goToPage(url, element) {
 
 document.querySelectorAll('.menu a, .full-container a').forEach(link => {
   link.addEventListener('click', function(event) {
-    event.preventDefault(); // empêche le comportement par défaut du lien
+    event.preventDefault(); // empêche le comportement par défaut du lien, important pour avoir les anims
     const url = this.getAttribute('href'); // récupère lien
     const container = this.closest('.full-container'); // récupère le conteneur parent
     goToPage(url, container); // déclenche la transition de page
   });
 });
 
-// reset les styles des containers + exec la fonction playTimeline lorsque btn nav est cliqué
+// reset le style de container + exec la fonction playTimeline lorsque btn nav est cliqué
 window.addEventListener('pageshow', (event) => {
   if (event.persisted) {
-    resetFullContainers();
+    resetFullContainer();
     playTimeline();
   }
 });
