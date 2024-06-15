ab375a5e1cd1164d4ee05f3c4f9a6ce182575792
Louislf <lefrancois_louis@yahoo.fr>
2024-06-15 18:32:33 +0200

    Update gsap-works.js

--- src/gsap-works.js
+++ src/gsap-works.js
@@ -1,62 +1,43 @@
-const textsection = document.querySelector('.text-section');
+const textSection = document.querySelector('.text-section');
 const transition = document.querySelector('.transition');
 
+// Réinitialise les propriétés des éléments animés
 function resetAnimations() {
-  // Réinitialiser les propriétés des éléments animés
-  gsap.set(textsection, { clearProps: "all" });
-  gsap.set(transition, { clearProps: "all" });
+  gsap.set(textSection, { clearProps: 'all' });
+  gsap.set(transition, { clearProps: 'all' });
 }
 
+// Démarre les animations
 function startAnimations() {
-  gsap.timeline({
-    onComplete: function () {
-      textsection.style.visibility = 'visible';
-      textsection.style.mixBlendMode = 'difference';
+  // Assurez-vous que les timelines ne se chevauchent pas
+  const tl = gsap.timeline({
+    onComplete: function() {
+      textSection.style.visibility = 'visible';
+      textSection.style.mixBlendMode = 'difference';
     }
   });
 
   if (window.innerWidth > 768) {
-    gsap.timeline().from('.text-section', { duration: 1, x: window.innerWidth, ease: 'power2.out' }, 1.2);
+    tl.from('.text-section', { duration: 1, x: window.innerWidth, ease: 'power2.out' }, 1.2);
   }
 
-  gsap.timeline().from('.transition', { duration: 1.2, scaleY: -1, y: -window.innerHeight, transformOrigin: 'bottom', ease: 'power4.inOut' }, .5);
+  tl.from('.transition', { duration: 1.2, scaleY: -1, y: -window.innerHeight, transformOrigin: 'bottom', ease: 'power4.inOut' }, 0.5);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  startAnimations();
-});
-
-window.addEventListener('pageshow', function(event) {
-  if (event.persisted) {
-    // La page a été restaurée depuis le cache
-    resetAnimations(); // Réinitialiser les propriétés avant de relancer les animations
-    startAnimations();
-  }
-});
-
+// Gère la transition de page avec animation
 function goToPage(url) {
-  gsap.to('.transition', { duration: 1, scaleY: 1, transformOrigin: 'bottom', ease: 'power4.inOut', onComplete: () => { window.location.href = url; }
+  gsap.to('.transition', {
+    duration: 1,
+    scaleY: 1,
+    transformOrigin: 'bottom',
+    ease: 'power4.inOut',
+    onComplete: () => {
+      window.location.href = url;
+    }
   });
 }
 
-document.querySelectorAll('.logo a, .menu a, .thanks a.next-project').forEach(link => {
-  link.addEventListener('click', function (event) {
-    event.preventDefault(); // empêche le comportement par défaut du lien
-    const url = this.getAttribute('href'); // récupère lien
-    goToPage(url); // déclenche la transition de page
-  });
-});
-
-// Écouteur d'événement pour détecter le retour en arrière du navigateur
-window.addEventListener('popstate', function(event) {
-  // Vérifie si l'événement popstate est déclenché par un retour en arrière du navigateur
-  if (event.state && event.state.isBack) {
-    resetAnimations(); // Réinitialiser les propriétés avant de relancer les animations
-    startAnimations(); // Redémarre les animations lors du retour en arrière
-  }
-});
-
-// Fonction pour ajouter l'état de navigation
+// Gère la transition de page avec animation et ajoute un état de navigation
 function goToPageWithState(url) {
   gsap.to('.transition', {
     duration: 1,
@@ -70,11 +51,32 @@ function goToPageWithState(url) {
   });
 }
 
-// Mise à jour des écouteurs d'événements avec la nouvelle fonction goToPageWithState
+// Initialise les animations au chargement de la page
+document.addEventListener('DOMContentLoaded', function() {
+  startAnimations();
+});
+
+// Réinitialise et relance les animations lorsque la page est restaurée depuis le cache
+window.addEventListener('pageshow', function(event) {
+  if (event.persisted) {
+    resetAnimations(); // Réinitialise les propriétés
+    startAnimations(); // Relance les animations
+  }
+});
+
+// Réinitialise et relance les animations lors du retour en arrière du navigateur
+window.addEventListener('popstate', function(event) {
+  if (event.state && event.state.isBack) {
+    resetAnimations(); // Réinitialise les propriétés
+    startAnimations(); // Relance les animations
+  }
+});
+
+// Ajoute des écouteurs d'événements aux liens pour gérer les transitions de page
 document.querySelectorAll('.logo a, .menu a, .thanks a.next-project').forEach(link => {
   link.addEventListener('click', function(event) {
-    event.preventDefault(); // empêche le comportement par défaut du lien
-    const url = this.getAttribute('href'); // récupère le lien
-    goToPageWithState(url); // déclenche la transition de page avec état de navigation
+    event.preventDefault(); // Empêche le comportement par défaut du lien
+    const url = this.getAttribute('href'); // Récupère le lien
+    goToPageWithState(url); // Déclenche la transition de page avec état de navigation
   });
 });
