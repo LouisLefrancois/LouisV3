86c6629c510dd9d6f33fbb88cc67434461899dbb
Louislf <lefrancois_louis@yahoo.fr>
2024-06-15 17:48:50 +0200

    Update gsap-index.js

--- gsap-index.js
+++ gsap-index.js
@@ -3,7 +3,15 @@ const navbar = document.querySelector('.navbar');
 const scrolldowntxt = document.querySelector('.scroll-down');
 const buttondarkmode = document.querySelector('.buttondarkmode');
 
+function resetFullContainers() {
+  fullcontainer.forEach(container => {
+    gsap.set(container, { x: 0, opacity: 1 });
+  });
+}
+
 window.addEventListener('load', () => {
+  resetFullContainers();
+
   const TL = gsap.timeline({ paused: true });
   TL
       .from(navbar, .2, { top: 10, opacity: 0 }, .5) //  durée d'exec , sec avant exec
@@ -13,18 +21,24 @@ window.addEventListener('load', () => {
   TL.play();
 });
 
-// 2 arg : url de destination + elmnt a anim
 function goToPage(url, element) {
-    gsap.to(element, { duration: .75, x: -window.innerWidth, opacity: 0, ease: 'power2.inOut', onComplete: () => {
-      window.location.href = url;
-    }});
+  gsap.to(element, { duration: .75, x: -window.innerWidth, opacity: 0, ease: 'power2.inOut', onComplete: () => {
+    window.location.href = url;
+  }});
 }
-  
+
 document.querySelectorAll('.menu a, .full-container a').forEach(link => {
   link.addEventListener('click', function(event) {
-    event.preventDefault(); // empêche le comportement par défaut du lien, pour pouvoir voir les anims
+    event.preventDefault(); // empêche le comportement par défaut du lien
     const url = this.getAttribute('href'); // récupère lien
     const container = this.closest('.full-container'); // récupère le conteneur parent
     goToPage(url, container); // déclenche la transition de page
   });
+});
+
+// Réinitialise les styles des conteneurs lorsque l'utilisateur utilise les boutons du navigateur
+window.addEventListener('pageshow', (event) => {
+  if (event.persisted) {
+    resetFullContainers();
+  }
 });
