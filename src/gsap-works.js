f7320a9b8ad1b86659d070761d7545ab3af6e2af
Louislf <lefrancois_louis@yahoo.fr>
2024-06-15 19:06:54 +0200

    Update gsap-works.js

--- src/gsap-works.js
+++ src/gsap-works.js
@@ -1,12 +1,13 @@
 const textsection = document.querySelector('.text-section');
 const transition = document.querySelector('.transition');
 
+// reset le style des éléments animés
 function resetAnimations() {
-  // Réinitialiser les propriétés des éléments animés
   gsap.set(textsection, { clearProps: "all" });
   gsap.set(transition, { clearProps: "all" });
 }
 
+// change le styles de textsection
 function startAnimations() {
   gsap.timeline({
     onComplete: function () {
@@ -22,18 +23,20 @@ function startAnimations() {
   gsap.timeline().from('.transition', { duration: 1.2, scaleY: -1, y: -window.innerHeight, transformOrigin: 'bottom', ease: 'power4.inOut' }, .5);
 }
 
+// exec la fonction startAnimations lorsque la page est chargée
 document.addEventListener('DOMContentLoaded', function () {
   startAnimations();
 });
 
+// reset les anims + les exec lorsque btn nav est cliqué
 window.addEventListener('pageshow', function(event) {
   if (event.persisted) {
-    // La page a été restaurée depuis le cache
-    resetAnimations(); // Réinitialiser les propriétés avant de relancer les animations
+    resetAnimations();
     startAnimations();
   }
 });
 
+// anim quand on part de la page, (quand on clique sur .logo a, .menu a ou .thanks a.next-project)
 function goToPage(url) {
   gsap.to('.transition', { duration: 1, scaleY: 1, transformOrigin: 'bottom', ease: 'power4.inOut', onComplete: () => { window.location.href = url; }
   });
@@ -47,16 +50,16 @@ document.querySelectorAll('.logo a, .menu a, .thanks a.next-project').forEach(li
   });
 });
 
-// Écouteur d'événement pour détecter le retour en arrière du navigateur
+// détecte le retour en arrière du navigateur
 window.addEventListener('popstate', function(event) {
-  // Vérifie si l'événement popstate est déclenché par un retour en arrière du navigateur
+  // verif si l'event popstate est déclenché par un retour en arrière du navigateur
   if (event.state && event.state.isBack) {
-    resetAnimations(); // Réinitialiser les propriétés avant de relancer les animations
-    startAnimations(); // Redémarre les animations lors du retour en arrière
+    resetAnimations();
+    startAnimations();
   }
 });
 
-// Fonction pour ajouter l'état de navigation
+// ajoute un état de nav
 function goToPageWithState(url) {
   gsap.to('.transition', {
     duration: 1,
@@ -70,7 +73,7 @@ function goToPageWithState(url) {
   });
 }
 
-// Mise à jour des écouteurs d'événements avec la nouvelle fonction goToPageWithState
+// met à jour goToPageWithState
 document.querySelectorAll('.logo a, .menu a, .thanks a.next-project').forEach(link => {
   link.addEventListener('click', function(event) {
     event.preventDefault(); // empêche le comportement par défaut du lien
