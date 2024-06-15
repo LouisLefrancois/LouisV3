17e83be7cbb099b21ff4ce34657cca02b7b130ad
Louislf <lefrancois_louis@yahoo.fr>
2024-06-15 19:13:24 +0200

    Update gsap-contact.js

--- src/gsap-contact.js
+++ src/gsap-contact.js
@@ -1,35 +1,75 @@
-const firstline = document.querySelector('.first-line');
-const secondline = document.querySelector('.second-line');
-const mailcontainer = document.querySelector('.mail-container');
+const firstline = document.querySelector(".first-line");
+const secondline = document.querySelector(".second-line");
+const mailcontainer = document.querySelector(".mail-container");
 
-document.addEventListener('DOMContentLoaded', function() {
-  gsap.timeline({
-    onComplete: function() {
-      firstline.style.visibility = 'visible';
-      secondline.style.visibility = 'visible';
-      mailcontainer.style.visibility = 'visible';
-    }
-  });
+function resetAnimations() {
+  gsap.set(".first-line", { x: 0 });
+  gsap.set(".second-line", { x: 0 });
+  gsap.set(".mail-container", { x: 0 });
+}
+
+function startAnimations() {
+  gsap
+    .timeline({
+      onComplete: function () {
+        firstline.style.visibility = "visible";
+        secondline.style.visibility = "visible";
+        mailcontainer.style.visibility = "visible";
+      },
+    })
+    .from(
+      ".first-line",
+      { duration: 1, x: -window.innerWidth, ease: "power2.out" },
+      0
+    )
+    .from(
+      ".second-line",
+      { duration: 1.4, x: -window.innerWidth, ease: "power2.out" },
+      0
+    )
+    .from(
+      ".mail-container",
+      { duration: 1.75, x: -window.innerWidth, ease: "power2.out" },
+      0
+    );
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  startAnimations();
 });
 
-gsap.set('.first-section', {});
-
-gsap.timeline().from('.first-section', {})
-  .from('.first-line', { duration: 1, x: -window.innerWidth, ease: 'power2.out' }, 0)
-  .from('.second-line', { duration: 1.4, x: -window.innerWidth, ease: 'power2.out' }, 0)
-  .from('.mail-container', { duration: 1.75, x: -window.innerWidth, ease: 'power2.out' }, 0)
-
 function goToPage(url) {
-  gsap.to('.first-line', { duration: 1, x: -window.innerWidth, ease: 'power2.inOut'}, )
-  gsap.to('.second-line', { duration: 1.15, x: -window.innerWidth, ease: 'power2.inOut'}, )
-  gsap.to('.mail-container', { duration: 1.3, x: -window.innerWidth, ease: 'power2.inOut', onComplete: () => { window.location.href = url; }
+  gsap.to(".first-line", {
+    duration: 1,
+    x: -window.innerWidth,
+    ease: "power2.inOut",
+  });
+  gsap.to(".second-line", {
+    duration: 1.15,
+    x: -window.innerWidth,
+    ease: "power2.inOut",
+  });
+  gsap.to(".mail-container", {
+    duration: 1.3,
+    x: -window.innerWidth,
+    ease: "power2.inOut",
+    onComplete: () => {
+      window.location.href = url;
+    },
   });
 }
 
-document.querySelectorAll('.menu a').forEach(link => {
-  link.addEventListener('click', function(event) {
+document.querySelectorAll(".menu a").forEach((link) => {
+  link.addEventListener("click", function (event) {
     event.preventDefault(); // empêche le comportement par défaut du lien
-    const url = this.getAttribute('href'); // récupère lien
+    const url = this.getAttribute("href"); // récupère lien
     goToPage(url); // déclanche la transition de page
   });
+});
+
+window.addEventListener("pageshow", function (event) {
+  if (event.persisted) {
+    resetAnimations();
+    startAnimations();
+  }
 });
